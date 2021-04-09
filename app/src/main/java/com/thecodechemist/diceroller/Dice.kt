7024aa75d381963835234393f78f04865508dc8a
TheKemistLabs <thekemistlabs@gmail.com>
2021-04-09 15:12:21 +0100

    Added an additional die color of Red and updated methods to get correct image resourced based on color and number rolled. This will be later fully implemented through and options menu that the user can access to personalise the dice used.

--- app/src/main/java/com/thecodechemist/diceroller/Dice.kt
+++ app/src/main/java/com/thecodechemist/diceroller/Dice.kt
@@ -9,4 +9,34 @@ class Dice(private val numSides: Int, private val color: String) {
     fun roll(): Int {
         return (1..numSides).random()
     }
+
+    fun getDiceImage(rolledNum: Int): Int {
+        //Determine which dice image should be displayed
+        var imageResource: Int
+
+        if(this.color == "Black") {
+            imageResource = when(rolledNum) {
+                1 -> R.drawable.black_die_1
+                2 -> R.drawable.black_die_2
+                3 -> R.drawable.black_die_3
+                4 -> R.drawable.black_die_4
+                5 -> R.drawable.black_die_5
+                else -> R.drawable.black_die_6
+            }
+
+        } else if(this.color == "Red") {
+            imageResource = when(rolledNum) {
+                1 -> R.drawable.red_die_1
+                2 -> R.drawable.red_die_2
+                3 -> R.drawable.red_die_3
+                4 -> R.drawable.red_die_4
+                5 -> R.drawable.red_die_5
+                else -> R.drawable.red_die_6
+            }
+        } else {
+            return 0;
+        }
+
+        return imageResource
+    }
 }
--- app/src/main/java/com/thecodechemist/diceroller/MainActivity.kt
+++ app/src/main/java/com/thecodechemist/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.thecodechemist.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 /*
@@ -24,23 +23,18 @@ class MainActivity : AppCompatActivity() {
     // Sets a random number as the TextView based on a dice roll
     private fun rollDice() {
         //Create a dice with 6 sides and roll it
-        val dice = Dice(6, "Black")
+        val dice = Dice(6, "Red")
         val diceRoll = dice.roll()
 
         //Determine which dice image should be displayed
-        val imageResource = when(diceRoll) {
-            1 -> R.drawable.die_1
-            2 -> R.drawable.die_2
-            3 -> R.drawable.die_3
-            4 -> R.drawable.die_4
-            5 -> R.drawable.die_5
-            else -> R.drawable.die_6
-        }
+        val imageResource = dice.getDiceImage(diceRoll)
 
         //Display the dice image and set the content description to the number rolled
         val resultImageView: ImageView = findViewById(R.id.imageView)
         resultImageView.setImageResource(imageResource)
         resultImageView.contentDescription = diceRoll.toString()
     }
+
+
 }
 
